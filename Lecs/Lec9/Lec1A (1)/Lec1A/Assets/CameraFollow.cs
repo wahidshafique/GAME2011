90e5d5b800a2e1dc319abcb41eaf193008bdbbcb
wahidshafique <wahidshafique@hotmail.com>
2016-03-14 22:28:16 -0400

    add current lec

---
+++ Lecs/Lec9/Lec1A (1)/Lec1A/Assets/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform ball;
+
+
+	public void LateUpdate() {
+		transform.position = ball.position;
+	}
+
+}
---
+++ Lecs/Lec9/Lec1A (1)/Lec1A/Assets/MonkeyBall.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkeyBall : MonoBehaviour {
+
+	public float minTilt = 5.0f;
+	private Vector3 totalRotation = Vector3.zero;
+	public Rigidbody body;
+
+	// Use this for initialization
+	public void Awake() {
+		Input.gyro.enabled = true;
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 rotation = Input.gyro.rotationRate * Mathf.Rad2Deg;
+		if (Mathf.Abs (rotation.x) < minTilt)
+			rotation.x = 0;
+		if (Mathf.Abs (rotation.y) < minTilt)
+			rotation.y = 0;
+		if (Mathf.Abs (rotation.z) < minTilt)
+			rotation.z = 0;
+		totalRotation += new Vector3 (-rotation.x,
+			rotation.z, -rotation.y) * Time.deltaTime;
+	}
+
+
+	void FixedUpdate() {
+		body.AddTorque (totalRotation);	
+	}
+}
