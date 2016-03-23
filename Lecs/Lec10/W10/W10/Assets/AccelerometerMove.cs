f60b54f791cb687d8519b62be9f89e776444881e
wahidshafique <wahidshafique@hotmail.com>
2016-03-22 22:59:53 -0400

    added some labs

---
+++ Lecs/Lec10/W10/W10/Assets/AccelerometerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float accelerationX = Input.acceleration.x;
+		float accelerationY = Input.acceleration.y;
+		float accelerationZ = Input.acceleration.z;
+		//transform.Translate (temp, 0, 0);
+		transform.Translate(0,0,-15*accelerationX*Time.deltaTime);
+		transform.Rotate(0,0,-accelerationZ);
+	
+	}
+}
